ae039e2368c051953c7d654b2d17a13dbdb266d9
dnyaneshwarrathod245 <rdnyaneshwar947@gmail.com>
2026-06-25 00:44:23 +0530

    Added Netflix clone

---
+++ object.js
@@ -0,0 +1,26 @@
+let Rohit={
+    t20i:{
+        Matches:159,
+        Runs:4231,
+        BattingAverage:32.05,
+        StrikeRate:140,
+        Highestscore:121,
+
+        
+    },
+    ODI:{
+        totalmatches:260,
+        totalruns:16000,
+
+    },
+    TEST:{
+           totalmatches:47,
+           totalruns:4342,
+           
+    }
+}
+
+
+    console.log(Rohit);
+    
+    
